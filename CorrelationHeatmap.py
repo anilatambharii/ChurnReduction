fdc904c5f691cf97bf319e3a5297a336971fc3a4
anilatambharii <anil@ambharii.com>
2025-03-28 17:16:47 -0400

    changes

--- CorrelationHeatmap.py
+++ CorrelationHeatmap.py
@@ -1,6 +1,8 @@
+import pandas as pd  # Required for pd.read_csv()
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('sample_churn_data.csv')
 sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
 plt.show()
+
